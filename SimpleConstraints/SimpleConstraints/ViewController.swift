da6429f6c2e5689cb426826b18d5bb304ea7e85b
Yumin Chu <yuminc03@gmail.com>
2025-12-28 23:21:40 +0900

    feat: workspace에 SimpleConstraints 프로젝트 추가

---
+++ SimpleConstraints/SimpleConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleConstraints
+//
+//  Created by Yumin Chu on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
