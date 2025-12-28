a0e77ceb3b8fe328b50a63536c2e3d3f7f772756
Yumin Chu <yuminc03@gmail.com>
2025-12-28 23:22:47 +0900

    feat: workspace에 VarietySolutionsExample 프로젝트 추가

---
+++ VarietySolutionsExample/VarietySolutionsExample/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  MyView.swift
+//  VarietySolutionsExample
+//
+//  Created by Yumin Chu on 2023/03/12.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+
+}
---
+++ VarietySolutionsExample/VarietySolutionsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VarietySolutionsExample
+//
+//  Created by Yumin Chu on 2023/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
