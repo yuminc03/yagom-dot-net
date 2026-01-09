e9c0d92f8da91551abc533c8c40a331dd7d2e79d
Yumin Chu <yuminc03@gmail.com>
2026-01-09 11:43:23 +0900

    refactor: LottoMachine과 StrangeCalculator 테스트 분리

--- UnitTestSample Starter/LottoMachinTests/LottoMachinTests.swift
+++ UnitTestSample Starter/LottoMachinTests/LottoMachinTests.swift
@@ -9,28 +9,127 @@ import XCTest
 @testable import UnitTestSample
 
 final class LottoMachinTests: XCTestCase {
+  var sut: LottoMachine!
   
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    sut = LottoMachine()
   }
   
   override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    sut = nil
   }
   
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    // Any test you write for XCTest can be annotated as throws and async.
-    // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-    // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+  func test_isValidLottoNumbers호출시_6개보다적은숫자입력했을때_False를반환하는지() {
+    // given
+    let input = [3, 6, 9]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
   }
   
-  func testPerformanceExample() throws {
-    // This is an example of a performance test case.
-    measure {
-      // Put the code you want to measure the time of here.
-    }
+  func test_isValidLottoNumbers호출시_6개보다많은숫자입력했을때_False를반환하는지() {
+    // given
+    let input = [3, 6, 9, 12, 15, 18, 21, 24]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
   }
   
+  func test_isValidLottoNumbers호출시_중복해서6개가되는_6개보다많은숫자를입력했을때_False를반환하는지() {
+    // given
+    let input = [3, 6, 9, 12, 15, 18, 15, 9]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
+  }
+  
+  func test_isValidLottoNumbers호출시_중복된숫자없이_6개숫자입력했을때_True를반환하는지() {
+    // given
+    let input = [3, 6, 9, 12, 15, 18]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertTrue(result)
+  }
+  
+  func test_isValidLottoNumbers호출시_중복된숫자가있는_6개숫자입력했을때_False를반환하는지() {
+    // given
+    let input = [3, 6, 9, 9, 15, 18]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
+  }
+  
+  func test_isValidLottoNumbers호출시_1보다작은수를포함하는배열전달했을때_False를반환하는지() {
+    // given
+    let input = [0, 3, 6, 9, 12, 15]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
+  }
+  
+  func test_isValidLottoNumbers호출시_45보다큰수를포함하는배열전달했을때_False를반환하는지() {
+    // given
+    let input = [3, 6, 9, 12, 15, 50]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertFalse(result)
+  }
+  
+  func test_isValidLottoNumbers호출시_모든수가1부터45범위에포함되는배열전달했을때_True를반환하는지() {
+    // given
+    let input = [1, 5, 15, 25, 35, 45]
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: input)
+    
+    // then
+    XCTAssertTrue(result)
+  }
+  
+  func test_makeRandomLottoNumbersArray_반환배열이유효한지() {
+    // given
+    let randomNumbers = sut.makeRandomLottoNumbersArray()
+    
+    // when
+    let result = sut.isValidLottoNumbers(of: randomNumbers)
+    
+    // then
+    XCTAssertTrue(result)
+  }
+  
+  func test_countMatchingNumber_몇개의번호를맞추었는지() throws {
+    // given
+    let randomNumbers = sut.makeRandomLottoNumbersArray()
+    let winnerNumbers = sut.makeRandomLottoNumbersArray()
+    
+    // when
+    let result = try sut.countMatchingNumber(
+      user: randomNumbers,
+      winner: winnerNumbers
+    )
+    
+    // then
+    XCTAssertNoThrow(result)
+  }
 }
--- UnitTestSample Starter/StrangeCalculatorTests/StrangeCalculatorTests.swift
+++ UnitTestSample Starter/StrangeCalculatorTests/StrangeCalculatorTests.swift
@@ -11,20 +11,17 @@ import XCTest
 final class StrangeCalculatorTests: XCTestCase {
   
   var sut: StrangeCalculator!
-  var lotto: LottoMachine!
   
   override func setUpWithError() throws {
     try super.setUpWithError()
     
     sut = StrangeCalculator()
-    lotto = LottoMachine()
   }
   
   override func tearDownWithError() throws {
     try super.tearDownWithError()
     
     sut = nil
-    lotto = nil
   }
   
   func test_addNumbers() {
@@ -36,119 +33,5 @@ final class StrangeCalculatorTests: XCTestCase {
     
     // then
     XCTAssertEqual(result, 33)
-  }
-  
-  func test_isValidLottoNumbers호출시_6개보다적은숫자입력했을때_False를반환하는지() {
-    // given
-    let input = [3, 6, 9]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_6개보다많은숫자입력했을때_False를반환하는지() {
-    // given
-    let input = [3, 6, 9, 12, 15, 18, 21, 24]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_중복해서6개가되는_6개보다많은숫자를입력했을때_False를반환하는지() {
-    // given
-    let input = [3, 6, 9, 12, 15, 18, 15, 9]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_중복된숫자없이_6개숫자입력했을때_True를반환하는지() {
-    // given
-    let input = [3, 6, 9, 12, 15, 18]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertTrue(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_중복된숫자가있는_6개숫자입력했을때_False를반환하는지() {
-    // given
-    let input = [3, 6, 9, 9, 15, 18]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_1보다작은수를포함하는배열전달했을때_False를반환하는지() {
-    // given
-    let input = [0, 3, 6, 9, 12, 15]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_45보다큰수를포함하는배열전달했을때_False를반환하는지() {
-    // given
-    let input = [3, 6, 9, 12, 15, 50]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertFalse(result)
-  }
-  
-  func test_isValidLottoNumbers호출시_모든수가1부터45범위에포함되는배열전달했을때_True를반환하는지() {
-    // given
-    let input = [1, 5, 15, 25, 35, 45]
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: input)
-    
-    // then
-    XCTAssertTrue(result)
-  }
-  
-  func test_makeRandomLottoNumbersArray_반환배열이유효한지() {
-    // given
-    let randomNumbers = lotto.makeRandomLottoNumbersArray()
-    
-    // when
-    let result = lotto.isValidLottoNumbers(of: randomNumbers)
-    
-    // then
-    XCTAssertTrue(result)
-  }
-  
-  func test_countMatchingNumber_몇개의번호를맞추었는지() throws {
-    // given
-    let randomNumbers = lotto.makeRandomLottoNumbersArray()
-    let winnerNumbers = lotto.makeRandomLottoNumbersArray()
-    
-    // when
-    let result = try lotto.countMatchingNumber(
-      user: randomNumbers,
-      winner: winnerNumbers
-    )
-    
-    // then
-    XCTAssertNoThrow(result)
   }
 }
