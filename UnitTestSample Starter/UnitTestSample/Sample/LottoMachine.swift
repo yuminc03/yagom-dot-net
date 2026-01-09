59bea7249d5486f59f82b27a21126793eec5ea72
Yumin Chu <yuminc03@gmail.com>
2026-01-09 11:06:27 +0900

    chore: LottoMachine 구조체 메서드에 주석 추가

--- UnitTestSample Starter/UnitTestSample/Sample/LottoMachine.swift
+++ UnitTestSample Starter/UnitTestSample/Sample/LottoMachine.swift
@@ -6,43 +6,45 @@
 import Foundation
 
 struct LottoMachine {
-    func isValidLottoNumbers(of numbers: [Int]) -> Bool {
-        guard numbers.count == 6, Set(numbers).count == 6 else {
-            return false
-        }
-        
-        for num in numbers {
-            guard 1...45 ~= num else {
-                return false
-            }
-        }
-        
-        return true
+  // numbers가 유효한 lotto 번호인지 검증
+  func isValidLottoNumbers(of numbers: [Int]) -> Bool {
+    guard numbers.count == 6, Set(numbers).count == 6 else {
+      return false
     }
     
-    func makeRandomLottoNumbersArray() -> [Int] {
-        var numberSet: Set<Int> = []
-        
-        while numberSet.count < 6 {
-            let randomNumber = Int.random(in: 1...45)
-            numberSet.insert(randomNumber)
-        }
-        
-        return Array(numberSet)
+    for num in numbers {
+      guard 1...45 ~= num else {
+        return false
+      }
     }
     
+    return true
+  }
+  
+  // 랜덤으로 lotto 번호 6자리를 배열로 리턴
+  func makeRandomLottoNumbersArray() -> [Int] {
+    var numberSet: Set<Int> = []
     
-    func countMatchingNumber(user: [Int], winner: [Int]) throws -> Int {
-        guard isValidLottoNumbers(of: user) && isValidLottoNumbers(of: winner) else {
-            throw LottoMachineError.invalidNumbers
-        }
-        
-        let winNumbers = user.filter { winner.contains($0) }
-        return winNumbers.count
+    while numberSet.count < 6 {
+      let randomNumber = Int.random(in: 1...45)
+      numberSet.insert(randomNumber)
     }
+    
+    return Array(numberSet)
+  }
+  
+  // 1등 번호와 사용자의 번호가 얼마나 일치하는지 리턴
+  func countMatchingNumber(user: [Int], winner: [Int]) throws -> Int {
+    guard isValidLottoNumbers(of: user) && isValidLottoNumbers(of: winner) else {
+      throw LottoMachineError.invalidNumbers
+    }
+    
+    let winNumbers = user.filter { winner.contains($0) }
+    return winNumbers.count
+  }
 }
 
 enum LottoMachineError: Error {
-    case invalidNumbers
+  case invalidNumbers
 }
 
